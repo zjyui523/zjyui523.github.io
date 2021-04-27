8f642768d795d9a0c7d05c65026cc8a16ee4ab10
zjyui523 <zj523@icloud.com>
2021-04-27 16:46:30 +0800

    测试git

---
+++ HelloWorld/src/main/java/com/java1234/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.java1234.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/helloWorld")
+	public String say(){
+		return "spring boot 你大爷";
+	}
+}
